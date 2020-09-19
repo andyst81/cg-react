e4bd570c0afe284192f0a69542e284540a88eb57
andy <andyst81@users.noreply.github.com>
2020-09-19 15:37:30 +0700

    Edit styling around title

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function IndexPage() {
     <div>
       <Nav />
       <div className="container mx-auto py-20">
-        <h1 className="text-5xl text-center text-accent-1">
+        <h1 className="text-5xl text-center text-accent-1 pb-8">
           CoinGecko Data Simple Viewer
         </h1>
         <CoinGecko />
