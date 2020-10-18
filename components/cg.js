0764f5d1453defdf1d700968291c865db8db814d
andy <andyst81@users.noreply.github.com>
2020-10-18 23:11:58 +0700

    add some better styling

--- components/cg.js
+++ components/cg.js
@@ -48,7 +48,7 @@ class CoinGecko extends React.Component {
   setInput = (key) => (event) => {
     this.setState(prevState => ({ [key]: !prevState.[key] }));
   };
-  // Function to set the percentage shown 
+  // Function to set the percentage shown
   setPercentage = (key) => (event) => {
      this.setState({ [key]: event.target.value})
   };
@@ -77,51 +77,51 @@ class CoinGecko extends React.Component {
           {/* Show other options button */}
           <div className='flex items-center justify-center'>
             <Link href="/">
-              <a className="bg-transparent hover:bg-purple-600 text-purple-600 font-semibold 
-                hover:text-white py-2 px-4 border border-purple-600 hover:border-transparent rounded" 
+              <a className="bg-transparent hover:bg-purple-600 text-purple-600 font-semibold
+                hover:text-white py-2 px-4 border border-purple-600 hover:border-transparent rounded"
                 onClick={this.setInput('allShow')}>{this.state.allShow ? 'Hide' : 'Show'} Options</a>
             </Link>
           </div>
-          <form id='setting' className={"grid grid-cols-3 gap-4 py-2 " + (this.state.allShow ? '' : 'checked')}> 
+          <form id='setting' className={"grid grid-cols-3 gap-4 py-2 " + (this.state.allShow ? '' : 'checked')}>
             {/* Logic to arrange the hourly data */}
-            <div className='border-solid border-2 border-grey-500 rounded p-4 text-center'>
+            <div className='border-solid border-2 border-purple-300 rounded p-4 text-center'>
               <h1 className="text-xl font-semibold underline">Hourly</h1>
               <span>Hourly percentage ></span>
               <input type='number' className='px-4 py-3 w-32' placeholder='Show all' name='hourPercent' value={this.props.value}
-                onChange={this.setPercentage('hourPercent')}></input>
-              <br/>              
+                onChange={this.setPercentage('hourPercent')}></input> %
+              <br/>
               <input type="checkbox" id="hour" name="hour" value="1 Hour" onChange={this.setInput('hourShow')} />
               <label className='px-4 py-3' htmlFor="hour">Remove hourly % change</label>
             </div>
             {/* Logic to arrange the daily data */}
-            <div className='border-solid border-2 border-grey-500 rounded p-4 text-center'>
+            <div className='border-solid border-2 border-purple-300 rounded p-4 text-center'>
               <h1 className="text-xl font-semibold underline">Daily</h1>
               <span>Daily percentage ></span>
               <input type='number' className='px-4 py-3 w-32' placeholder='Show all' name='dayPercent' value={this.props.value}
-                onChange={this.setPercentage('dayPercent')}></input>
+                onChange={this.setPercentage('dayPercent')}></input> %
               <br/>
               <input type="checkbox" id="day" name="day" value="1 Day" onChange={this.setInput('dayShow')} />
               <label htmlFor="day">Remove daily % change</label>
             </div>
             {/* Logic to arrange the weekly data */}
-            <div className='border-solid border-2 border-grey-500 rounded p-4 text-center'>
+            <div className='border-solid border-2 border-purple-300 rounded p-4 text-center'>
               <h1 className="text-xl font-semibold underline">Weekly</h1>
               <span>Weekly percentage ></span>
-              <input type='number' className='px-4 py-3 w-32' placeholder='Show all' name='weekPercent' 
+              <input type='number' className='px-4 py-3 w-32' placeholder='Show all' name='weekPercent'
               value={this.props.value} onChange={this.setPercentage('weekPercent')}></input>
               <br/>
               <input type="checkbox" id="week" name="week" value="1 Week" onChange={this.setInput('weekShow')} />
-              <label htmlFor="week">Remove weekly % change</label>
+              <label htmlFor="week">Remove weekly % change</label> %
             </div>
           </form>
           {/* Reset button */}
           <div className={'flex items-center justify-center ' + (this.state.allShow ? '' : 'checked')}>
           <Link href="/">
-              <a className="bg-transparent hover:bg-red-600 text-red-600 font-semibold 
-                hover:text-white py-2 px-4 border border-red-600 hover:border-transparent rounded" 
+              <a className="bg-transparent hover:bg-red-600 text-red-600 font-semibold
+                hover:text-white py-2 px-4 border border-red-600 hover:border-transparent rounded"
                 onClick={() => this.resetScreen()}>Reset Screen</a>
             </Link>
-            </div>          
+            </div>
 
           <div className='container mx-auto flex items-center justify-center'>
             <table className='table-auto'>
@@ -139,18 +139,18 @@ class CoinGecko extends React.Component {
               <tbody>
                 {data.map(item => (
                   <tr key={item.name} className={
-                    ( !this.state.weekPercent ? null : item.price_change_percentage_7d_in_currency >= 
-                      this.state.weekPercent ? '' : ' checked ' ) + 
-                    ( !this.state.dayPercent ? null : item.price_change_percentage_24h_in_currency >= 
+                    ( !this.state.weekPercent ? null : item.price_change_percentage_7d_in_currency >=
+                      this.state.weekPercent ? '' : ' checked ' ) +
+                    ( !this.state.dayPercent ? null : item.price_change_percentage_24h_in_currency >=
                       this.state.dayPercent ? '' : ' checked ' ) +
-                    ( !this.state.hourPercent ? null : item.price_change_percentage_1h_in_currency >= 
+                    ( !this.state.hourPercent ? null : item.price_change_percentage_1h_in_currency >=
                       this.state.hourPercent ? '' : ' checked ' )
                   }>
                     <td className="border px-4 py-2">{item.market_cap_rank}</td>
-                    <td className="border px-4 py-2 items-center"><img className='justify-center' src={item.image} 
+                    <td className="border px-4 py-2 items-center"><img className='justify-center' src={item.image}
                       alt='coin name' width='20' /></td>
                     <td className="border px-4 py-2">
-                        <a className='text-purple-600 font-medium hover:text-red-800' 
+                        <a className='text-purple-600 font-medium hover:text-red-800'
                           href={'https://www.coingecko.com/en/coins/' + item.id}>
                           {item.name}
                         </a>
@@ -158,17 +158,17 @@ class CoinGecko extends React.Component {
                     {/* A JSX comment */}
                     <td className="border px-4 py-2">${Math.round(item.current_price * 1000) / 1000}</td>
                     <td className={( this.state.hourShow ? '' : 'checked ' ) +
-                      ( item.price_change_percentage_1h_in_currency >= 0 ? 'text-green-500 border px-4 py-2' : 
+                      ( item.price_change_percentage_1h_in_currency >= 0 ? 'text-green-500 border px-4 py-2' :
                       'text-red-500 border px-4 py-2' )}>{Math.round(item.price_change_percentage_1h_in_currency * 100) / 100}%
                     </td>
                     {/* A JSX comment */}
                     <td className={( this.state.dayShow ? '' : 'checked ' ) +
-                      ( item.price_change_percentage_24h_in_currency >= 0 ? 'text-green-500 border px-4 py-2' : 
+                      ( item.price_change_percentage_24h_in_currency >= 0 ? 'text-green-500 border px-4 py-2' :
                       'text-red-500 border px-4 py-2' )}>{Math.round(item.price_change_percentage_24h_in_currency * 100) / 100}%
                     </td>
                     {/* A JSX comment */}
                     <td className={( this.state.weekShow ? '' : 'checked ' ) +
-                      (item.price_change_percentage_7d_in_currency >= 0 ? 'text-green-500 border px-4 py-2' : 
+                      (item.price_change_percentage_7d_in_currency >= 0 ? 'text-green-500 border px-4 py-2' :
                       'text-red-500 border px-4 py-2' )}>{Math.round(item.price_change_percentage_7d_in_currency * 100) / 100}%
                     </td>
                   </tr>
