16ad8a4aeff88e1f61e7193d4550218c27a8d021
andy <andyst81@users.noreply.github.com>
2020-11-09 21:55:04 +0700

    Edit coins pages

--- pages/coins/[query].js
+++ pages/coins/[query].js
@@ -10,7 +10,7 @@ const Posts = ({ results: query }) => {
             <div className="container mx-auto" key={index}>
               <div className='flex items-center justify-center pb-4'>
                 <img src={q.image.small} alt='Coin Image' />
-                <h1 className='text-5xl text-center text-accent-1'> {q.coingecko_rank}. {q.name} </h1>
+                <h1 className='text-5xl text-center text-accent-1'> {q.market_cap_rank}. {q.name} </h1>
                 <img src={q.image.small} alt='Coin Image' />
                 <br/>
                 
@@ -19,23 +19,29 @@ const Posts = ({ results: query }) => {
               
               <p className="text-3xl text-center">Current Price: ${q.market_data.current_price.usd}</p>
               <br />
-              <div className="flex text-xl items-center space-x-4">
-                <p>Current Market Sentiment</p>
-                <p className="text-green-600 text-center"> {q.sentiment_votes_up_percentage}</p>
+              <div className="flex text-xl items-center space-x-3">
+                <p>Current Market Sentiment: </p>
+                
                 <svg xmlns="http://www.w3.org/2000/svg" height='14px' width='14px' className='self-center' viewBox="0 0 20 20">
-                    <path d="M11.0010436,0 C9.89589787,0 9.00000024,0.886706352 9.0000002,1.99810135 L9,8 L1.9973917,8 C0.894262725,8 0,8.88772964 0,10 L0,12 L2.29663334,18.1243554 C2.68509206,19.1602453 3.90195042,20 5.00853025,20 L12.9914698,20 C14.1007504,20 15,19.1125667 15,18.000385 L15,10 L12,3 L12,0 L11.0010436,0 L11.0010436,0 Z M17,10 L20,10 L20,20 L17,20 L17,10 L17,10 Z" id="Fill-97"></path>              
+                  <path d="M11.0010436,0 C9.89589787,0 9.00000024,0.886706352 9.0000002,1.99810135 L9,8 L1.9973917,8 C0.894262725,8 0,8.88772964 0,10 L0,12 L2.29663334,18.1243554 C2.68509206,19.1602453 3.90195042,20 5.00853025,20 L12.9914698,20 C14.1007504,20 15,19.1125667 15,18.000385 L15,10 L12,3 L12,0 L11.0010436,0 L11.0010436,0 Z M17,10 L20,10 L20,20 L17,20 L17,10 L17,10 Z" id="Fill-97"></path>              
                 </svg>
-                <p className="text-red-600 text4-center"> {q.sentiment_votes_down_percentage}</p>
+                <p className="text-green-600 text-center"> {q.sentiment_votes_up_percentage ? q.sentiment_votes_up_percentage : "None"}</p>
+                
                 <svg xmlns="http://www.w3.org/2000/svg" height='14px' width='14px' className='self-center' viewBox="0 0 20 20">
-                        <path d="M11.0010436,20 C9.89589787,20 9.00000024,19.1132936 9.0000002,18.0018986 L9,12 L1.9973917,12 C0.894262725,12 0,11.1122704 0,10 L0,8 L2.29663334,1.87564456 C2.68509206,0.839754676 3.90195042,8.52651283e-14 5.00853025,8.52651283e-14 L12.9914698,8.52651283e-14 C14.1007504,8.52651283e-14 15,0.88743329 15,1.99961498 L15,10 L12,17 L12,20 L11.0010436,20 L11.0010436,20 Z M17,10 L20,10 L20,0 L17,0 L17,10 L17,10 Z" id="Fill-97"></path>
-                  </svg>
+                  <path d="M11.0010436,20 C9.89589787,20 9.00000024,19.1132936 9.0000002,18.0018986 L9,12 L1.9973917,12 C0.894262725,12 0,11.1122704 0,10 L0,8 L2.29663334,1.87564456 C2.68509206,0.839754676 3.90195042,8.52651283e-14 5.00853025,8.52651283e-14 L12.9914698,8.52651283e-14 C14.1007504,8.52651283e-14 15,0.88743329 15,1.99961498 L15,10 L12,17 L12,20 L11.0010436,20 L11.0010436,20 Z M17,10 L20,10 L20,0 L17,0 L17,10 L17,10 Z" id="Fill-97"></path>
+                </svg>
+                <p className="text-red-600 text4-center"> {q.sentiment_votes_down_percentage ? q.sentiment_votes_down_percentage  : "None"}</p>
               </div>
               <br />
-              <p className="text-xl">All Time High: ${q.market_data.ath.usd}</p>
+              <p className="text-xl">All Time High Price: ${q.market_data.ath.usd}</p>
+              <br />
+              <p className="text-xl">Market Cap: ${q.market_data.market_cap.usd.toLocaleString()}</p>
+              <br />
+              <p className="text-xl"> Price Change in 24 Hours: ${q.market_data.price_change_24h.toFixed(3)}</p>
               <br />
               <p className="text-xl">Website: <a href={q.links.homepage}>{q.links.homepage}</a></p>
               <br />
-              <p className="" dangerouslySetInnerHTML={{__html: q.description.en}}></p>
+              <p className="space-y-2" dangerouslySetInnerHTML={{__html: q.description.en}}></p>
             </div>
         ))}
     </div>
